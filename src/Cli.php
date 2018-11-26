441d0f71015964015d1af019e7a8dc4fe7194c83
Alexey Abramov <li0n0km0nk@gmail.com>
2018-11-26 12:28:49 +0300

    add prg:cli-tools and create function run

---
+++ src/Cli.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * User: li0n0k
+ * Date: 26.11.18
+ * Time: 12:03
+ */
+
+namespace Project1;
+
+use function cli\line;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+function run()
+{
+    line('Welcome to the Brain Game!' . PHP_EOL);
+    $name = \cli\prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
